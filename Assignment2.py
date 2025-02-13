19db80add57d15bc7120adf52b6239e8352905c5
N-Heyer <156978640+N-Heyer@users.noreply.github.com>
2025-02-13 16:47:05 -0600

    added open for file

--- Assignment2.py
+++ Assignment2.py
@@ -1,2 +1,4 @@
-import re
+import re #import regex
 
+with open("Assignment2_Test_File.txt", "r") as file: #opens specfic file to read over
+    lines = [line.strip() for line in file] # strips the file
